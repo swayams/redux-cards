e32ada1b380e7b133976713c6e9949a5251d6f78
Swayam Siddha <swayam.siddha@bitwiseglobal.com>
2019-03-03 21:46:25 +0530

    count

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component} from 'react';
 import { connect } from 'react-redux'
 const mapStateToProps = (state) => {
-    return {count: state.articles.count}
+    return {count: state.articles.length}
 }
 class ConnectedCounter extends Component {
     constructor() {
